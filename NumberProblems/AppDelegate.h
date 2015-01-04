62b4bdcf3674dc7e3d702433da7bb60d372c2399
Kevin Lambert <kevinslambert@hotmail.com>
2015-01-04 15:49:01 -0700

    Initial Commit

---
+++ NumberProblems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NumberProblems
+//
+//  Created by Kevin Lambert on 1/4/15.
+//  Copyright (c) 2015 KSL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NumberProblems/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NumberProblems
+//
+//  Created by Kevin Lambert on 1/4/15.
+//  Copyright (c) 2015 KSL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
